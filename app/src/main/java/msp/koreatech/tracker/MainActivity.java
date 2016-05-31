f547dd4589fc0a77c161227e1a95afe4b5d1cd53
JeongHeon <overwhelm07>
2016-05-31 16:48:37 +0900

    gradle version update

--- app/src/main/java/msp/koreatech/tracker/MainActivity.java
+++ app/src/main/java/msp/koreatech/tracker/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
