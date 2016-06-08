1b1afb2bd68ff3dd8af7929fbf9d838dec8e0f47
iwasahorse <iwasahorse@gmail.com>
2016-06-09 08:28:06 +0900

    horse #3

--- app/src/main/java/msp/koreatech/tracker/MainActivity.java
+++ app/src/main/java/msp/koreatech/tracker/MainActivity.java
@@ -3,15 +3,25 @@ package msp.koreatech.tracker;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String ACTION_GPS_UPDATE = "msp.koreatech.tracker.gps";
     private Intent intentService;
     private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
+            Toast.makeText(MainActivity.this, "GPS 전달 완료", Toast.LENGTH_SHORT).show();
+            double longitude = intent.getDoubleExtra("longitude", 0);
+            double latitude = intent.getDoubleExtra("latitude", 0);
 
+
+            TextView textGPS = (TextView) findViewById(R.id.textGPS);
+            textGPS.setText(String.format("longitude: %f, latitude: %f", longitude, latitude));
         }
     };
     @Override
@@ -19,18 +29,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(ACTION_GPS_UPDATE);
         intentService = new Intent(this, PeriodicMonitorService.class);
+        registerReceiver(broadcastReceiver, intentFilter);
+        startService(intentService);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        startService(intentService);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         stopService(intentService);
+        unregisterReceiver(broadcastReceiver);
     }
 }
--- app/src/main/java/msp/koreatech/tracker/PeriodicMonitorService.java
+++ app/src/main/java/msp/koreatech/tracker/PeriodicMonitorService.java
@@ -7,75 +7,100 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.location.GpsStatus;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.Looper;
-import android.os.SystemClock;
 import android.util.Log;
 import android.widget.Toast;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 import android.os.Handler;
 
-public class PeriodicMonitorService extends Service implements GpsStatus.Listener {
+public class PeriodicMonitorService extends Service  {
     private static final String TAG = "Tracker";
     private static final int ALARM_INTERVAL = 1000 * 5;
     private static final int GPS_TIMEOUT = 1000 * 5;
     private static final String ACTION_ALARM = "msp.koreatech.tracker.alarm";
     private static final String ACTION_GPS_UPDATE = "msp.koreatech.tracker.gps";
     private LocationManager locationManager = null;
-    private GpsStatus gpsStatus;
+    private Intent intentMain;
+    private WifiManager wifiManager;
     private AlarmManager alarmManager;
     private PendingIntent alarmIntent;
     private Looper looper;
     private boolean isRequestRegistered = false;
-    private double longitude = 0.0;
-    private double latitude = 0.0;
+    private boolean wifiScanning = false;
 
     private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            if(intent.getAction().equals(ACTION_ALARM)) {
+            String stringAction = intent.getAction();
+            if(stringAction.equals(ACTION_ALARM)) {
                 Log.d(TAG, "방송");
-                if(gpsStatus != null && gpsStatus.getTimeToFirstFix() == 0)
-                    Log.d(TAG, "GPS를 이용할 수 없음");
-                else {
-                    Intent intentMain = new Intent(context, MainActivity.class);
-                    intentMain.setAction(ACTION_GPS_UPDATE);
-                    intentMain.putExtra("longitude", longitude);
-                    intentMain.putExtra("latitude", latitude);
-                    sendBroadcast(intentMain);
-                }
                 try {
-                    locationManager.removeUpdates(locationListener);
-                    locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, locationListener, null);
+                    locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, locationListener, looper);
                 } catch (SecurityException e) {
                     e.printStackTrace();
                 }
             }
+            if(stringAction.equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)) {
+                Log.d(TAG, "SCAN_RESULTS_AVAILABLE_ACTION");
+                checkProximity();
+                wifiScanning = false;
+
+            }
         }
     };
 
+    private Runnable runnableRemoveUpdates = new Runnable() {
+        public void run() {
+            if(locationManager != null && locationListener != null) {
+                if (locationManager.getGpsStatus(null).getTimeToFirstFix() == 0 && !wifiScanning) {
+                    wifiScanning = true;
+                    wifiManager.startScan();   //Wi-Fi
+                    Log.d(TAG, "start Wi-Fi scanning");
+                }else
+                    Log.d(TAG, "TTFF: " + locationManager.getGpsStatus(null).getTimeToFirstFix() + ", scanning: " + wifiScanning);
+                try {
+                    locationManager.removeUpdates(locationListener);
+                } catch (SecurityException e) {
+                    e.printStackTrace();
+                }
+            }
+            else
+                Log.d(TAG, "locationManager or locationListener problem");
+        }
+    };
 
     private LocationListener locationListener = new LocationListener() {
         @Override
         public void onLocationChanged(Location location) {
-            Log.d(TAG, " Time : " + getCurrentTime() + " Longitude : " + location.getLongitude()
-                    + " Latitude : " + location.getLatitude() + " Altitude: " + location.getAltitude()
-                    + " Accuracy : " + location.getAccuracy());
+            double longitude;
+            double latitude;
+            float accuracy = location.getAccuracy();
+
             longitude = location.getLongitude();
             latitude = location.getLatitude();
+            Log.d(TAG, " Time : " + getCurrentTime() + " Longitude : " + longitude
+                    + " Latitude : " + latitude + " Altitude: " + location.getAltitude()
+                    + " Accuracy : " + location.getAccuracy());
+            intentMain.putExtra("longitude", longitude);
+            intentMain.putExtra("latitude", latitude);
+            intentMain.putExtra("accuracy", accuracy);
+            sendBroadcast(intentMain);
         }
 
         @Override
         public void onStatusChanged(String provider, int status, Bundle extras) {
             Log.d(TAG, "GPS status changed.");
-            Toast.makeText(getApplicationContext(), "GPS status changed.", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(getApplicationContext(), status + ": GPS status changed.", Toast.LENGTH_SHORT).show();
         }
 
         @Override
@@ -86,32 +111,30 @@ public class PeriodicMonitorService extends Service implements GpsStatus.Listene
         @Override
         public void onProviderDisabled(String provider) {
             Log.d(TAG, "GPS onProviderDisabled: " + provider);
-            Toast.makeText(getApplicationContext(), "GPS is off, please turn on!", Toast.LENGTH_LONG).show();
+            Toast.makeText(PeriodicMonitorService.this, "GPS is off, please turn on!", Toast.LENGTH_LONG).show();
         }
     };
 
     @Override
     public void onCreate() {
         Log.d(TAG, "onCreate");
-        Intent intent =  new Intent(ACTION_ALARM);
-        IntentFilter intentFilter = new IntentFilter(ACTION_ALARM);
-        registerReceiver(broadcastReceiver, intentFilter);  Toast.makeText(this, "Monitor 시작", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Monitor 시작", Toast.LENGTH_SHORT).show();
+
+        Intent intentAlarm =  new Intent(ACTION_ALARM);
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(ACTION_ALARM);
+        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        intentMain = new Intent(ACTION_GPS_UPDATE);
+        registerReceiver(broadcastReceiver, intentFilter);
         requestLocation();
+        wifiManager = (WifiManager)getSystemService(WIFI_SERVICE);
         alarmManager = (AlarmManager)this.getSystemService(Context.ALARM_SERVICE);
-        alarmIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
-        alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime(),
+        alarmIntent = PendingIntent.getBroadcast(this, 0, intentAlarm, 0);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 5000,
                 ALARM_INTERVAL, alarmIntent);
         looper = Looper.myLooper();
         final Handler handler = new Handler(looper);
-        handler.postDelayed(new Runnable() {
-            public void run() {
-                try {
-                    locationManager.removeUpdates(locationListener);
-                } catch (SecurityException e) {
-                    e.printStackTrace();
-                }
-            }
-        }, GPS_TIMEOUT);
+        handler.postDelayed(runnableRemoveUpdates, GPS_TIMEOUT);
     }
 
     @Override
@@ -119,19 +142,18 @@ public class PeriodicMonitorService extends Service implements GpsStatus.Listene
         // intent: startService() 호출 시 넘기는 intent 객체
         // flags: service start 요청에 대한 부가 정보. 0, START_FLAG_REDELIVERY, START_FLAG_RETRY
         // startId: start 요청을 나타내는 unique integer id
-
         Log.d(TAG, "onStartCommand");
-
 
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy() {
+        Log.d(TAG, "onDestroy");
         Toast.makeText(this, "Monitor 중지", Toast.LENGTH_SHORT).show();
-        cancelLocationRequest();
         unregisterReceiver(broadcastReceiver);
         alarmManager.cancel(alarmIntent);
+        cancelLocationRequest();
     }
 
     @Override
@@ -139,44 +161,19 @@ public class PeriodicMonitorService extends Service implements GpsStatus.Listene
         return null;
     }
 
-    @Override
-    public void onGpsStatusChanged(int event) {
-        if(locationManager != null)
-            gpsStatus = locationManager.getGpsStatus(null);
-        /*switch (event) {
-            case GpsStatus.GPS_EVENT_STARTED:
-                Log.d(TAG, "GPS_EVENT_STARTED");
-                break;
-
-            case GpsStatus.GPS_EVENT_STOPPED:
-                Log.d(TAG, "GPS_EVENT_STOPPED");
-                break;
-
-            case GpsStatus.GPS_EVENT_FIRST_FIX:
-                Log.d(TAG, "GPS_EVENT_FIRST_FIX");
-                break;
-
-            case GpsStatus.GPS_EVENT_SATELLITE_STATUS:
-                Log.d(TAG, "GPS_EVENT_SATELLITE_STATUS");
-                break;
-        }
-        Log.d( TAG, "TimeToFirstFix: " +  gpsStatus.getTimeToFirstFix() );
-        */
-    }
 
     private void requestLocation() {
         try {
             if(locationManager == null) {
                 Log.d(TAG, "LocationManager obtained");
                 locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-                locationManager.addGpsStatusListener(this);
             }
             if(!isRequestRegistered) {
                 /*locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
                         MIN_TIME_UPDATES,
                         MIN_DISTANCE_CHANGE_FOR_UPDATES,
                         locationListener);*/
-                locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, locationListener, null);
+                locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, locationListener, looper);
                 isRequestRegistered = true;
             }
         } catch (SecurityException se) {
@@ -187,16 +184,28 @@ public class PeriodicMonitorService extends Service implements GpsStatus.Listene
 
     private void cancelLocationRequest() {
         Log.d(TAG, "Cancel the location update request");
-        if(locationManager != null) {
-            try {
-                locationManager.removeUpdates(locationListener);
-            } catch(SecurityException se) {
-                se.printStackTrace();
-                Log.e(TAG, "PERMISSION_NOT_GRANTED");
-            }
-        }
+        runnableRemoveUpdates.run();
         locationManager = null;
         isRequestRegistered = false;
+    }
+
+    private void checkProximity() {
+        List<ScanResult> scanList = wifiManager.getScanResults();
+        boolean isProximate = false;
+
+        for(int i = 1; i < scanList.size(); i++) {
+            ScanResult result = scanList.get(i);
+            Log.d(TAG, "SSID: " + result.SSID + ", BSSID: " + result.BSSID + ", RSSI: " + result.level );
+            //if(top1APId.equals(result.SSID+result.BSSID) && result.level > top1rssi - 10)    //로직
+            if(true)
+                isProximate = true;
+        }
+
+        if(isProximate) {
+            //장소 진입
+        } else {
+            //장소 진입 X
+        }
     }
 
     public static String getCurrentTime() {
